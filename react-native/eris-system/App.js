6f2c9e9f3a353c692a2ebb9849eaa39da62fcaf7
Arandelle <topaguintsarandell@gmail.com>
2024-07-06 19:18:03 +0800

    Added nativewind, update the style of login form using the native wind

--- react-native/eris-system/App.js
+++ react-native/eris-system/App.js
@@ -1,23 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import LoginForm from './LoginForm';
+import { Text, View } from 'react-native';
+import LoginForm from './src/components/LoginForm';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text>Login to your account</Text>
       <LoginForm />
       <StatusBar style="auto" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-    padding: 20,
-  },
-});
