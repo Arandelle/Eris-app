a48474a5d2899450a3ab452f26841d850ff06be6
Arandelle <topaguintsarandell@gmail.com>
2024-08-21 19:12:34 +0800

    Added image container for profile

--- Eris-app/src/screens/Profile.js
+++ Eris-app/src/screens/Profile.js
@@ -11,6 +11,7 @@ import {
   TouchableOpacity,
   Modal,
   TouchableWithoutFeedback,
+  Image
 } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import { ref, onValue } from "firebase/database";
@@ -102,6 +103,10 @@ const Profile = ({ setIsProfileComplete }) => {
       <View className="flex-1 justify-between bg-white rounded-lg m-4 p-5 shadow-md ">
         <View>
           <View className="items-center pb-5">
+           {userData?.img ? (<Image source={{uri: userData.img}} 
+             style={{ width: 100, height: 100, borderRadius: 50 }}
+           />) : 
+          ( <Text className="text-gray-900 text-lg">Image not available</Text>)}
             <Text className="text-2xl font-bold pb-2">
               {userData?.firstname && userData?.lastname
                 ? `${userData.firstname} ${userData.lastname}`
