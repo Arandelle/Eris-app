227b159bd266dc8fcae6b73b303e968e85968a66
Arandelle <topaguintsarandell@gmail.com>
2024-10-12 09:59:37 +0800

    Install image zoom viewer to view the image

--- Eris-app/src/screens/Home.js
+++ Eris-app/src/screens/Home.js
@@ -1,16 +1,18 @@
 import { useEffect, useState } from "react";
-import { View, Image, Text, ScrollView, Alert, RefreshControl } from "react-native";
+import { View, Image, Text, ScrollView, Alert, RefreshControl, TouchableOpacity, Modal } from "react-native";
 import ProfileReminderModal from "../component/ProfileReminderModal";
 import { ref, onValue } from "firebase/database";
 import { database } from "../services/firebaseConfig";
 import { formatDate } from "../helper/FormatDate";
 import { getTimeDifference } from "../helper/getTimeDifference";
-import logo from "../../assets/logo.png";
+import ImageViewer from 'react-native-image-zoom-viewer'
 
 const Home = ({ setShowTabBar }) => {
   const [announcement, setAnnouncement] = useState([]);
   const [scrollOffset, setScrollOffset] = useState(0);
   const [refreshing, setRefreshing] = useState(false); // To track refresh state
+  const [isImageModalVisible, setIsImageModalVisible] = useState(false); // State to control modal visibility
+  const [selectedImageUri, setSelectedImageUri] = useState(""); // State to hold the image URI to be shown in modal
 
   const handleScroll = (event) => {
     let currentOffset = event.nativeEvent.contentOffset.y;
@@ -18,7 +20,13 @@ const Home = ({ setShowTabBar }) => {
     setScrollOffset(currentOffset);
     setShowTabBar(direction === "up");
   };
+      // Function to handle pulling down to refresh
+      const handleRefresh = () => {
+        setRefreshing(true); // Set refreshing to true
+        fetchAnnouncements(); // Reload announcements
+      };
 
+      
   const fetchAnnouncements = () => {
     const announcementRef = ref(database, `announcement`);
     onValue(announcementRef, (snapshot) => {
@@ -45,63 +53,82 @@ const Home = ({ setShowTabBar }) => {
     fetchAnnouncements(); // Load announcements when the component mounts
   }, []);
 
-    // Function to handle pulling down to refresh
-    const handleRefresh = () => {
-      setRefreshing(true); // Set refreshing to true
-      fetchAnnouncements(); // Reload announcements
+    const handleImageClick = (imageUri) => {
+      setSelectedImageUri(imageUri);
+      setIsImageModalVisible(true); // Show the image modal
     };
 
   return (
-    <ScrollView
-      onScroll={handleScroll}
-      scrollEventThrottle={16} // Ensures smooth scrolling events
-      contentContainerStyle={{ flexGrow: 1 }}
-      refreshControl={
-        <RefreshControl
-          refreshing={refreshing}
-          onRefresh={handleRefresh} // Trigger refresh when the user pulls down
-        />
-      }
-    >
-      <View className="flex-1 px-3 pb-3 bg-white space-y-3">
-        <ProfileReminderModal />
-
-        {announcement.length > 0 &&
-          announcement.map((item, key) => (
-            <View
-              key={key}
-              className="rounded-lg bg-white border-0.5 border-gray-800 shadow-2xl"
-            >
-              <Image
-                source={{ uri: item.imageUrl }}
-                className=" h-52 rounded-t-lg"
-                resizeMode="cover"
+    <>
+       {/* Image Viewer Modal */}
+       {isImageModalVisible && (
+        <Modal
+          visible={isImageModalVisible}
+          transparent={true}
+          onRequestClose={() => setIsImageModalVisible(false)} // Close modal when back button is pressed
+        >
+             <ImageViewer
+                imageUrls={[{ url: selectedImageUri }]} // Use selected image URL for viewing
+                onSwipeDown={() => setIsImageModalVisible(false)} // Close modal on swipe down
+                enableSwipeDown={true}
+                renderIndicator={() => null} // Hide the pagination
+                backgroundColor='rgba(0,0,0,0.8)'
               />
-              <View className="p-4 space-y-2">
-                <Text className="font-bold text-blue-500">
-                  {formatDate(item.startDate)}
-                </Text>
-                <Text className="font-bold text-lg">{item.title}</Text>
-                <Text className="text-gray-600 text-lg">
-                  {item.description}
-                </Text>
-                <View className="pt-2 flex flex-row items-center space-x-3">
+        </Modal>
+      )}
+      <ScrollView
+        onScroll={handleScroll}
+        scrollEventThrottle={16} // Ensures smooth scrolling events
+        contentContainerStyle={{ flexGrow: 1 }}
+        refreshControl={
+          <RefreshControl
+            refreshing={refreshing}
+            onRefresh={handleRefresh} // Trigger refresh when the user pulls down
+          />
+        }
+      >
+        <View className="flex-1 px-3 pb-3 bg-white space-y-3">
+          <ProfileReminderModal />
+  
+          {announcement.length > 0 &&
+            announcement.map((item, key) => (
+              <View
+                key={key}
+                className="rounded-lg bg-white border-0.5 border-gray-800 shadow-2xl"
+              >
+               <TouchableOpacity onPress={() => handleImageClick(item.imageUrl)}>
                   <Image
-                    source={{
-                      uri: "https://flowbite.com/docs/images/people/profile-picture-5.jpg",
-                    }}
-                    className="h-10 w-10 rounded-full"
+                    source={{ uri: item.imageUrl }}
+                    className=" h-52 rounded-t-lg"
+                    resizeMode="cover"
                   />
-                  <View>
-                    <Text className="font-bold text-blue-500">Admin</Text>
-                    <Text>{getTimeDifference(item.timestamp)}</Text>
+               </TouchableOpacity>
+                <View className="p-4 space-y-2">
+                  <Text className="font-bold text-blue-500">
+                    {formatDate(item.startDate)}
+                  </Text>
+                  <Text className="font-bold text-lg">{item.title}</Text>
+                  <Text className="text-gray-600 text-lg">
+                    {item.description}
+                  </Text>
+                  <View className="pt-2 flex flex-row items-center space-x-3">
+                    <Image
+                      source={{
+                        uri: "https://flowbite.com/docs/images/people/profile-picture-5.jpg",
+                      }}
+                      className="h-10 w-10 rounded-full"
+                    />
+                    <View>
+                      <Text className="font-bold text-blue-500">Admin</Text>
+                      <Text>{getTimeDifference(item.timestamp)}</Text>
+                    </View>
                   </View>
                 </View>
               </View>
-            </View>
-          ))}
-      </View>
-    </ScrollView>
+            ))}
+        </View>
+      </ScrollView>
+    </>
   );
 };
 
