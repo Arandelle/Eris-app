449deb05e7cc11ca93d34415cb5e7fcc54b07cd4
Arandelle <topaguintsarandell@gmail.com>
2024-10-27 16:34:17 +0800

    parameterize shared fields like date,timestamp and isSeen, and use spread operator to add this on other data

--- Eris-app/src/hooks/useSendNotification.jsx
+++ Eris-app/src/hooks/useSendNotification.jsx
@@ -7,41 +7,40 @@ const useSendNotification = (emergencyType, description) => {
   const { currentUser } = useCurrentUser();
 
   const sendNotification = async (dataType, userId, messageType) => {
-    const notificationMessage = {
-      userProfileUpdate: {
-        title: "Profile Updated!",
-        message: "Your profile was updated successfully.",
+
+    const mainNotificationData = {
         isSeen: false,
         date: new Date().toISOString(),
         timestamp: serverTimestamp(),
+    }
+
+    const notificationMessage = {
+      userProfileUpdate: {
+        ...mainNotificationData,
+        title: "Profile Updated!",
+        message: "Your profile was updated successfully.",
         icon: "account-check",
       },
       userReport: {
+        ...mainNotificationData,
         title: "Emergency Reported!",
         message: `You have successfully reported an emergency.`,
         description,
-        isSeen: false,
-        timestamp: serverTimestamp(),
         icon: "hospital-box",
-        date: new Date().toISOString(),
       },
       adminReport: {
+        ...mainNotificationData,
         userId: currentUser?.id,
         message: `User ${currentUser?.email} submitted an emergency: ${emergencyType}`,
         description,
-        isSeen: false,
-        date: new Date().toISOString(),
-        timestamp: serverTimestamp(),
         icon: "hospital-box",
       },
       responderReport: {
+        ...mainNotificationData,
         userId: currentUser?.id,
         message: `New emergency reported from ${currentUser?.email}: ${emergencyType}`,
         description,
-        isSeen: false,
-        timestamp: serverTimestamp(),
         icon: "hospital-box",
-        date: new Date().toISOString(),
       },
     };
 
