3503e4464ed54254bf833b7b43e5eab1f1f1f66e
Arandelle Paguinto <topaguintsarandell@gmail.com>
2025-02-26 18:32:45 +0800

    update the navigation links in announcement

--- Eris-app/src/screens/Announcement.js
+++ Eris-app/src/screens/Announcement.js
@@ -37,7 +37,6 @@ const Announcement = () => {
     return adminData.find((user) => user.id === userId);
   };
 
-
   const handleTextLayout = (e, key) => {
     const { lines } = e.nativeEvent;
     if (lines.length > 2) {
@@ -54,14 +53,14 @@ const Announcement = () => {
         onRequestClose={closeImageModal} // Close viewer
       />
       <View className="space-y-3">
-      {loading ? (
-         <Text className="text-center text-gray-500 mt-3">Loading...</Text>
-      ) : slicedAnnouncement.length === 0 ? (
+        {loading ? (
+          <Text className="text-center text-gray-500 mt-3">Loading...</Text>
+        ) : slicedAnnouncement.length === 0 ? (
           <Text className="text-center text-gray-500 mt-3">
             No announcement available
-        </Text>
-      ) : (
-        slicedAnnouncement.length > 0 &&
+          </Text>
+        ) : (
+          slicedAnnouncement.length > 0 &&
           slicedAnnouncement.map((item, key) => {
             const adminDetails = getAdminsDetails(item.userId);
 
@@ -73,50 +72,57 @@ const Announcement = () => {
                 <TouchableOpacity
                   onPress={() => handleImageClick(item.fileUrl)}
                 >
-                {item.fileType === "image" && (
-                  <Image
-                    source={{ uri: item.fileUrl }}
-                    className=" h-52 rounded-t-lg"
-                    resizeMode="cover"
-                  />
-                )}
-                  
+                  {item.fileType === "image" && (
+                    <Image
+                      source={{ uri: item.fileUrl }}
+                      className=" h-52 rounded-t-lg"
+                      resizeMode="cover"
+                    />
+                  )}
                 </TouchableOpacity>
                 {item.fileType === "video" && (
-                 <View className="h-52 w-full relative">
-                  { videoLoading && 
-                  <View className="absolute inset-0 bg-black/50 flex items-center justify-center h-full w-full">
-                    <Text className="text-white text-center">Loading Video...</Text>
+                  <View className="h-52 w-full relative">
+                    {videoLoading && (
+                      <View className="absolute inset-0 bg-black/50 flex items-center justify-center h-full w-full">
+                        <Text className="text-white text-center">
+                          Loading Video...
+                        </Text>
+                      </View>
+                    )}
+                    <Video
+                      ref={videoRef}
+                      source={{ uri: item.fileUrl }}
+                      style={{
+                        width: "100%",
+                        height: 208,
+                        alignSelf: "center",
+                      }}
+                      useNativeControls
+                      resizeMode="contain"
+                      onLoadStart={() => setVideoLoading(true)}
+                      onLoad={() => setVideoLoading(false)}
+                      onError={() => setVideoLoading(false)}
+                    />
                   </View>
-                  }
-                   <Video 
-                   ref={videoRef}
-                    source={{uri: item.fileUrl}}
-                    style={{ width: "100%", height: 208, alignSelf: "center" }}
-                    useNativeControls
-                    resizeMode="contain"
-                    onLoadStart={() => setVideoLoading(true)}
-                    onLoad={() => setVideoLoading(false)}
-                    onError={() => setVideoLoading(false)}
-                   />
-                 </View>
                 )}
                 <View className="p-4 space-y-2">
-                  <Text className="font-bold text-xs text-blue-500">
+                  <Text className="font-bold text-xs text-gray-500">
                     {formatDate(item.date)}
                   </Text>
-                  <Pressable onPress={() => openLink(item.links, "links")}>
+                  <Pressable
+                    onPress={
+                      item.links ? () => openLink(item.links, "links") : null
+                    }
+                  >
                     <View className="flex-row items-center">
                       <Text
-                        className={`font-bold ${
-                          item.links ? "underline" : ""
-                        }`}
+                        className={`font-bold ${item.links ? "underline" : ""}`}
                       >
                         {item.title.toUpperCase()}
                       </Text>
                       {item.links && (
                         <Icon
-                          name="eye"
+                          name="link-variant"
                           size={20}
                           style={{ marginLeft: 5 }}
                           color={colors.gray[600]}
@@ -126,25 +132,34 @@ const Announcement = () => {
                   </Pressable>
 
                   <Text
-                  className="text-gray-600 text-justify"
-                  numberOfLines={expanded[key] ? undefined : 2}
-                  onTextLayout={(e) => handleTextLayout(e, key)}
-                >
-                  {item.description}
-                </Text>
-                {!expanded[key] && showSeeMore[key] && (
-                  <TouchableOpacity
-                    onPress={() => setExpanded((prev) => ({ ...prev, [key]: true }))}
+                    className="text-gray-600 text-justify"
+                    numberOfLines={expanded[key] ? undefined : 2}
+                    onTextLayout={(e) => handleTextLayout(e, key)}
                   >
-                    <Text className="text-blue-800 font-semibold">See more...</Text>
-                  </TouchableOpacity>
-                )}
-                {expanded[key] && (
-                  <TouchableOpacity onPress={() => setExpanded((prev) => ({...prev, [key]: false}))}>
-                    <Text className="text-gray-500 font-semibold">See less</Text>
-                  </TouchableOpacity>
-                )}
-
+                    {item.description}
+                  </Text>
+                  {!expanded[key] && showSeeMore[key] && (
+                    <TouchableOpacity
+                      onPress={() =>
+                        setExpanded((prev) => ({ ...prev, [key]: true }))
+                      }
+                    >
+                      <Text className="text-blue-800 font-semibold">
+                        Show full article...
+                      </Text>
+                    </TouchableOpacity>
+                  )}
+                  {expanded[key] && (
+                    <TouchableOpacity
+                      onPress={() =>
+                        setExpanded((prev) => ({ ...prev, [key]: false }))
+                      }
+                    >
+                      <Text className="text-gray-500 font-semibold">
+                        See less
+                      </Text>
+                    </TouchableOpacity>
+                  )}
 
                   <View className="pt-2 flex flex-row items-center space-x-3">
                     <TouchableOpacity
@@ -158,7 +173,7 @@ const Announcement = () => {
                       />
                     </TouchableOpacity>
                     <View>
-                      <Text className="font-bold text-blue-500">
+                      <Text className="font-bold text-blue-800">
                         {adminDetails?.fullname}
                       </Text>
                       <Text>{getTimeDifference(item.timestamp)}</Text>
@@ -168,12 +183,15 @@ const Announcement = () => {
               </View>
             );
           })
-      )}
-      {visibleCount < announcement.length && (
-        <TouchableOpacity onPress={loadMore} className="flex items-center justify-center p-2 rounded-lg">
-          <Text className="text-blue-800 text-lg">View More</Text>
-        </TouchableOpacity>
-      )}
+        )}
+        {visibleCount < announcement.length && (
+          <TouchableOpacity
+            onPress={loadMore}
+            className="flex items-center justify-center p-2 rounded-lg"
+          >
+            <Text className="text-blue-800 text-lg">View More</Text>
+          </TouchableOpacity>
+        )}
       </View>
     </>
   );
